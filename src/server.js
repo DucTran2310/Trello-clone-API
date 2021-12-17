4b0d843eea089b9e9d4cb8889cd6e397e9e66806
DucTran2310 <18520021@gm.uit.edu.vn>
2021-12-17 11:21:51 +0700

    Config mongodb, use env

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
-import express from "express";
-import { mapOrder } from "*/utilities/sort.js"
+import express from "express"
+import { connectDB } from '*/config/mongodb'
+import { env } from '*/config/environment'
 
-const app = express();
+const app = express()
 
-const hostName = 'localhost';
+// const hostName = 'localhost'
 
-const port = 8080;
+// const port = 8080
+
+connectDB().catch(console.log)
 
 app.get('/', (req, res) => {
   res.end('<h1>Hello world ADSTAR!!!</h1><hr/>')
 })
 
-app.listen(port, hostName, (req, res) => {
-  console.log(`Hello ADSTAR, Running at ${hostName}:${port}`)
+app.listen(env.PORT, env.HOST, (req, res) => {
+  console.log(`Hello ADSTAR, Running at ${env.HOST}:${env.PORT}`)
 })
